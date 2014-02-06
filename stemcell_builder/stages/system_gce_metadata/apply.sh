b2547db4b7f6589196c6385327ecd1bfc23b6fe4
Alexander Sologub <alexsologoub@gmail.com>
2014-02-06 14:02:22 +0300

    GCE Stemcell: Oh, almost forgot about metadata

---
+++ stemcell_builder/stages/system_gce_metadata/apply.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+base_dir=$(readlink -nf $(dirname $0)/../..)
+source $base_dir/lib/prelude_apply.bash
+
+cat >> $chroot/etc/hosts <<EOS
+169.254.169.254 metadata.google.internal metadata
+EOS
+"
